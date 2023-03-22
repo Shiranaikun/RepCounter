7cdd954721f5624f07a964441030a7959a587a32
Shiranaikun <121903198+Shiranaikun@users.noreply.github.com>
2023-03-22 15:25:24 +0100

    add files via upload
    first commit

---
+++ jsCode.js
@@ -0,0 +1,23 @@
+
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    console.log(count)
+    alert(count)
+
+    let history =  count + " - "
+    saveEl.textContent += history
+
+    count = 0
+    countEl.innerText = count
+}
+
+
+
